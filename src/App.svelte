699a93ad94d6b342bf81f98f6d22dc83b3b5d5de
uniboxx <unibox@duck.com>
2024-08-17 17:21:27 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import Cup from './lib/Cup.svelte';
+</script>
+
+<h1>Drink Water</h1>
+<h3>Goal: 2 Liters</h3>
+<div class="cup">
+  <div class="remained" id="remained">
+    <span id="liters">2L</span><small>Remained </small>
+  </div>
+  <div class="percentage" id="percentage"></div>
+</div>
+<p class="text">Select how many glasses of water that you have drank</p>
+<div class="cups">
+  {#each new Array(8) as cup, idx (idx)}
+    <Cup />
+  {/each}
+</div>
+
+<style lang="scss">
+  h3 {
+    font-weight: 400;
+    margin: 10px 0;
+  }
+
+  .text {
+    text-align: center;
+    margin: 0 0 20px;
+  }
+  .cups {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 280px;
+  }
+</style>
---
+++ src/lib/Cup.svelte
@@ -0,0 +1,28 @@
+<div class="cup cup-small">250 ml</div>
+
+<style lang="scss">
+  @use '../scss/vars';
+  .cup {
+    small {
+      font-size: 12px;
+    }
+
+    &.cup-small {
+      height: 95px;
+      width: 50px;
+      border-radius: 0 0 15px 15px;
+      background-color: rgba(255, 255, 255, 0.9);
+      cursor: pointer;
+      font-size: 14px;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      margin: 5px;
+      transition: 0.3s ease;
+      &.full {
+        background-color: vars.$fillColor;
+        color: #fff;
+      }
+    }
+  }
+</style>
